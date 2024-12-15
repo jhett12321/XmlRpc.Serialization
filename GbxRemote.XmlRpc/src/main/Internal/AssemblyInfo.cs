8f7bbed65fc613c818e6d258a9f2f01f97e8aefa
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2024-12-15 22:22:10 +0100

    Expose internals to test project. Add version info.

---
+++ GbxRemote.XmlRpc/src/main/Internal/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("GbxRemote.XmlRpc.Tests")]
+
+namespace GbxRemote.XmlRpc.Internal;
+
+internal static class AssemblyInfo
+{
+  public static readonly AssemblyInformationalVersionAttribute VersionInfo = typeof(AssemblyInfo).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()!;
+}
